e97dc3ba6f7d958ae3bbc14cdef22a8b9b01f6f5
kenjis <kenji.uui@gmail.com>
2016-10-08 13:42:57 +0900

    Add sample code for set_value()

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -3,23 +3,41 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 class Welcome extends CI_Controller {
 
-	/**
-	 * Index Page for this controller.
-	 *
-	 * Maps to the following URL
-	 * 		http://example.com/index.php/welcome
-	 *	- or -
-	 * 		http://example.com/index.php/welcome/index
-	 *	- or -
-	 * Since this controller is set as the default controller in
-	 * config/routes.php, it's displayed at http://example.com/
-	 *
-	 * So any other public methods not prefixed with an underscore will
-	 * map to /index.php/welcome/<method_name>
-	 * @see https://codeigniter.com/user_guide/general/urls.html
-	 */
-	public function index()
+	public function __construct()
+    {
+        parent::__construct();
+
+        $this->load->helper('url');
+    }
+
+    public function index()
 	{
 		$this->load->view('welcome_message');
 	}
+
+	public function set_value()
+    {
+        $this->load->helper('form');
+        echo set_value('text');
+    }
+
+    public function form_validation()
+    {
+        $this->load->library('form_validation');
+        $this->form_validation->set_rules('text', 'Text', 'required|max_length[1]');
+
+        echo 'before form_validation->run(): ';
+        echo set_value('text');
+        echo '<br>';
+
+        if ($this->form_validation->run()) {
+            echo 'after validation success: ';
+            echo set_value('text');
+            echo '<br>';
+        } else {
+            echo 'after validation failed: ';
+            echo set_value('text');
+            echo '<br>';
+        }
+    }
 }
