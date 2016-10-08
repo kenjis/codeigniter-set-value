6752c222599c12ec52cdd699f909cac811c75e4f
kenjis <kenji.uui@gmail.com>
2016-10-08 13:57:27 +0900

    Fix tests

--- application/tests/controllers/Welcome_test.php
+++ application/tests/controllers/Welcome_test.php
@@ -8,17 +8,17 @@ class Welcome_test extends TestCase
 		$this->assertEquals('&lt;s&gt;abc&lt;/s&gt;', $output);
 	}
 
-	public function test_form_validation_success()
+	public function test_form_validation_failed()
 	{
 		$output = $this->request('POST', 'welcome/form_validation', ['text' => '<s>abc</s>']);
 		$this->assertContains('before form_validation->run(): <br>', $output);
 		$this->assertContains('after validation failed: &lt;s&gt;abc&lt;/s&gt;<br>', $output);
 	}
 
-	public function test_form_validation_failed()
+	public function test_form_validation_success()
 	{
 		$output = $this->request('POST', 'welcome/form_validation', ['text' => 'x']);
 		$this->assertContains('before form_validation->run(): <br>', $output);
-		$this->assertContains('after validation failed: x<br>', $output);
+		$this->assertContains('after validation success: x<br>', $output);
 	}
 }
