043a7048573d7c5612e5a4efb59e647acbc07ce0
kenjis <kenji.uui@gmail.com>
2016-10-08 13:53:25 +0900

    Convert spaces to tabs

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -4,40 +4,40 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Welcome extends CI_Controller {
 
 	public function __construct()
-    {
-        parent::__construct();
+	{
+		parent::__construct();
 
-        $this->load->helper('url');
-    }
+		$this->load->helper('url');
+	}
 
-    public function index()
+	public function index()
 	{
 		$this->load->view('welcome_message');
 	}
 
 	public function set_value()
-    {
-        $this->load->helper('form');
-        echo set_value('text');
-    }
+	{
+		$this->load->helper('form');
+		echo set_value('text');
+	}
 
-    public function form_validation()
-    {
-        $this->load->library('form_validation');
-        $this->form_validation->set_rules('text', 'Text', 'required|max_length[1]');
+	public function form_validation()
+	{
+		$this->load->library('form_validation');
+		$this->form_validation->set_rules('text', 'Text', 'required|max_length[1]');
 
-        echo 'before form_validation->run(): ';
-        echo set_value('text');
-        echo '<br>';
+		echo 'before form_validation->run(): ';
+		echo set_value('text');
+		echo '<br>';
 
-        if ($this->form_validation->run()) {
-            echo 'after validation success: ';
-            echo set_value('text');
-            echo '<br>';
-        } else {
-            echo 'after validation failed: ';
-            echo set_value('text');
-            echo '<br>';
-        }
-    }
+		if ($this->form_validation->run()) {
+			echo 'after validation success: ';
+			echo set_value('text');
+			echo '<br>';
+		} else {
+			echo 'after validation failed: ';
+			echo set_value('text');
+			echo '<br>';
+		}
+	}
 }
